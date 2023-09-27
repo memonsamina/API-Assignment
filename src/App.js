71591fa23998056d9a7a51b74acefcb03184b629
Samina Memon <136779432+memonsamina@users.noreply.github.com>
2023-09-27 09:12:10 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import SecondMthod from './components/API';
+import FourthApi from './components/Fourth';
+import API from './components/Second';
+import APII from './components/Third';
+
+
+
+function App() {
+  return (
+    <>
+    
+    <SecondMthod/>
+    <API/>
+    <APII/>
+    <FourthApi/>
+
+    </>
+  );   
+}     
+
+export default App;
---
+++ src/components/Third.jsx
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const APII = () => {
+    const [PostMthod, setPostMthod] = useState([]);
+    const [UserData, setUserData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/todos").then((respo) => {
+            setPostMthod(respo.data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1 className="text fw-bold text-center"> MathOd-Three </h1>
+            <div
+                class="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                {UserData?.title}
+                            </h1>
+                            <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div class="modal-body">{UserData?.id}</div>
+                        <div class="modal-body">{UserData?.body}</div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button type="button" class="btn btn-primary">
+                                Save changes
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <table class="table table-info">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Body</th>
+                        <th scope="col"> Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {PostMthod.map((post) => {
+                        return (
+                            <tr key={post.id}>
+
+                                <td> {post.id} </td>
+                                <td>  {post.title}</td>
+                                <td> {post.body}</td>
+                                <td>
+
+                                    <button
+                                        type="button"
+                                        class="btn btn-color rounded-4 fw-bold"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal"
+                                        onClick={() => {
+                                            setUserData(post);
+
+                                        }}
+                                    >
+                                        View Data
+                                    </button>
+                                </td>
+                            </tr>
+
+                        );
+                    })}
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default APII;
